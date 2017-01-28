e5ae90b88b5732c94d3aec52b98d733b781e4164
Alex Begg <alex@medmen.com>
2017-01-27 18:01:41 -0800

    Modified so that for mymedmen-python it appends the destination

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-alert("Don't forget to run your tests!");
+if (document.location.href.indexOf("&dest") === -1 && document.location.href.indexOf("source=") > -1) {
+  document.location.href += "&dest=medmen/mymedmen-python::development";
+} else {
+  alert("Don't forget to run your tests!");
+}
 
